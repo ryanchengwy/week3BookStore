e822553bfdf312b002458dc54acb31a632a29f98
RyanC <ryan.chengwy@gmail.com>
2015-12-08 09:53:18 +0800

    Initial Commit

---
+++ week3BookStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week3BookStore
+//
+//  Created by Ryan on 8/12/2015.
+//  Copyright © 2015 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
